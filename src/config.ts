d0ee17f3eae64e289daac357571ceb2592da6ecf
WalnutBai <Walnuthyper@foxmail.com>
2026-06-04 17:38:32 +0800

    commit

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-﻿import type {
+import type {
 	AnnouncementConfig,
 	CommentConfig,
 	ExpressiveCodeConfig,
@@ -55,7 +55,7 @@ export const siteConfig: SiteConfig = {
 		// 显示模式："text-icon" 显示图标+文本，"logo" 仅显示Logo
 		mode: "text-icon",
 		// 顶栏标题文本
-		text: "MizukiUI",
+		text: "ShelterUI",
 		// 顶栏标题图标路径，默认使用 public/assets/home/home.webp
 		icon: "assets/home/home.webp",
 		// 网站Logo图片路径
--- src/data/friends.ts
+++ src/data/friends.ts
@@ -22,10 +22,10 @@ export const friendsData: FriendItem[] = [
 	},
 	{
 		id: 2,
-		title: "Mizuki Docs",
+		title: "Shelter Docs",
 		imgurl: "http://q.qlogo.cn/headimg_dl?dst_uin=3231515355&spec=640&img_type=jpg",
-		desc: "Mizuki User Manual",
-		siteurl: "https://docs.mizuki.mysqil.com",
+		desc: "Shelter User Manual",
+		siteurl: "https://docs.shelter.mysqil.com",
 		tags: ["Docs"],
 	},
 	{
